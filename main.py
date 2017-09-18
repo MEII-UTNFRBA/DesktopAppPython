a00f817e73358a56f689b54cef9c79a3f64ffb59
Francisco Dominguez <fdominguez@est.frba.utn.edu.ar>
2017-09-18 04:36:52 -0300

    Se emprolijo el codigo. Se agrego un nuevo boton que corresponde a la conexion con Arduino. Ahora, cuando se tocan alguno de los dos botones, se listan los equipos visa o los serie conectados dependiendo el caso

--- main.py
+++ main.py
@@ -8,36 +8,45 @@ from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.popup import Popup
 import visa
+from puerto_serie import serial_ports
+
+rm = visa.ResourceManager()
 
 
-class Main(ScreenManager):
+class Main(FloatLayout):
     pass
 
 
-class Gil(FloatLayout):
-    pass
+class VNAConnectPopup(Popup):
 
-
-class Monguito(Popup):
+    global rm
 
     def __init__(self, *args):
-        super(Monguito, self).__init__(*args)
-        box = BoxLayout()
-        for i in range(5):
-            box.add_widget(Button(
-                text = "Yes",
-                on_press = lambda *args: Monguito.hola(self)))
+        super(VNAConnectPopup, self).__init__(*args)
+        a=ObjectProperty()
+        a = rm.list_resources()
+        if len(a)>0:
+            box = BoxLayout(orientation='vertical')
+            for i in [len(a)]:
+                box.add_widget(Button(
+                    text=str(a[i-1]),
+                    on_press = lambda *args: VNAConnectPopup.hola(self,a[i-1])))
+            self.title = 'Seleccione un equipo'
+            self.content=box
+            self.size_hint=(0.4, 0.4)
+        else:
+            box = BoxLayout(orientation='vertical')
+            box.add_widget(Label(text='No hay equipo conectado'))
+            self.title = 'Error'
+            self.content=box
+            self.size_hint=(0.4, 0.4)
         box.add_widget(Button(
-                text = "Exit",
-                on_press = lambda *args: Monguito.chau(self)))
-
+            text="Cancelar",
+            on_press=lambda *args: VNAConnectPopup.chau(self)))
 #        box.add_widget(SubWidget())
 #        box.add_widget(SubWidget())
 #        box.add_widget(SubWidget())
 #        box.add_widget(SubWidget())
-        self.title='Test popup'
-        self.content=box
-        self.size_hint=(0.7, 0.7)
         self.auto_dismiss=False
         self.open()
 
@@ -45,57 +54,48 @@ class Monguito(Popup):
     def chau(self):
         self.dismiss()
 
-    def hola(self):
-        pass
+    def hola(self,*args):
+        print(args)
 
 
-class Trabajador(FloatLayout):
-    pass
-#    def instr_connect(self):
-#        global rm
-#        print(rm.list_resources())
-
-
-class Mongo(Popup):
-    pass
-
-
-class SubWidget(Button):
-
-    h = 'Mongo4'
-
-    def press_action(self):
-        print('esta entrando')
-        return lambda *args: Monguito.chau(Monguito.__self__)
-
-
-class MyWidget(Popup):
+class ArduinoConnectPopup(Popup):
 
     def __init__(self, *args):
-        super(MyWidget, self).__init__(*args)
-        self._create_widget()
+        super(ArduinoConnectPopup, self).__init__(*args)
+        a=serial_ports()
+        if len(a) > 0:
+            box = BoxLayout(orientation='vertical')
+            for i in [len(a)]:
+                box.add_widget(Button(
+                    text=str(a[i - 1]),
+                    on_press=lambda *args: ArduinoConnectPopup.hola(self,a[i - 1])))
+            self.title = 'Seleccione un equipo'
+            self.content = box
+            self.size_hint = (0.4, 0.4)
+        else:
+            box = BoxLayout(orientation='vertical')
+            box.add_widget(Label(text='No hay equipo conectado'))
+            self.title = 'Error'
+            self.content = box
+            self.size_hint = (0.4, 0.4)
 
-    def _create_widget(self):
-        box=BoxLayout()
-        box.add_widget(SubWidget(text='mongo1'))
-        box.add_widget(SubWidget(text='mongo2'))
-        box.add_widget(SubWidget(text='mongo3'))
-        box.add_widget(SubWidget(text='mongo4'))
-#        if self.widget is not None:
-#            self.remove_widget(self.widget)
-#        self.widget = SubWidget()
-#        self.add_widget(self.widget)
-#        self.widget = SubWidget()
-#        self.add_widget(self.widget)
-#        self.widget = SubWidget()
-#        self.add_widget(self.widget)
-#        popup = Popup(title='Test popup', content=box, size_hint=(None, None), size=(400, 400),auto_dismiss=False)
+        box.add_widget(Button(
+            text="Cancelar",
+            on_press=lambda *args: ArduinoConnectPopup.chau(self)))
+        self.auto_dismiss = False
+        self.open()
+
+    @staticmethod
+    def chau(self):
+        self.dismiss()
+
+    def hola(self,*args):
+        print(args)
 
 
 class MainApp(App):
     def build(self):
-#        return Gil()
-        return Gil()
+        return Main()
 
 
 if __name__ == '__main__':
