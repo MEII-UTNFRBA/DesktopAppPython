c5c9697e94ba8e25b3914a211ba047149dc17284
Francisco Dominguez <fdominguez@est.frba.utn.edu.ar>
2017-09-19 13:21:23 -0300

    Ya estaria el punto en el Smith. Se pusieron a forma de test botones arriba, abajo, izq, der

--- main.py
+++ main.py
@@ -9,17 +9,49 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.popup import Popup
 import visa
 from puerto_serie import serial_ports
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
 
 rm = visa.ResourceManager()
+vna_elegido = []
+puerto_arduino=[]
+inst=[]
 
 
 class Main(FloatLayout):
-    pass
+#    def __init__(self, name):
+#        self.name = name
+
+    punto = ObjectProperty(None)
+
+    def update(self):
+        self.punto.move()
+
+    def up(self):
+        self.punto.posicion_x =0
+        self.punto.posicion_y = 5
+        self.update()
+
+    def down(self):
+        self.punto.posicion_x =0
+        self.punto.posicion_y = -5
+        self.update()
+
+    def left(self):
+        self.punto.posicion_x =-5
+        self.punto.posicion_y = 0
+        self.update()
+
+    def right(self):
+        self.punto.posicion_x =5
+        self.punto.posicion_y = 0
+        self.update()
 
 
 class VNAConnectPopup(Popup):
 
-    global rm
+    global rm, vna_elegido, inst
 
     def __init__(self, *args):
         super(VNAConnectPopup, self).__init__(*args)
@@ -43,10 +75,6 @@ class VNAConnectPopup(Popup):
         box.add_widget(Button(
             text="Cancelar",
             on_press=lambda *args: VNAConnectPopup.chau(self)))
-#        box.add_widget(SubWidget())
-#        box.add_widget(SubWidget())
-#        box.add_widget(SubWidget())
-#        box.add_widget(SubWidget())
         self.auto_dismiss=False
         self.open()
 
@@ -55,10 +83,15 @@ class VNAConnectPopup(Popup):
         self.dismiss()
 
     def hola(self,*args):
-        print(args)
+        vna_elegido = args[0]
+#        inst = rm.open_resource(str(vna_elegido))      #Hay que ver como hacer para que no se rompa
+        print(str(vna_elegido))
+        self.dismiss()
 
 
 class ArduinoConnectPopup(Popup):
+
+    global puerto_arduino
 
     def __init__(self, *args):
         super(ArduinoConnectPopup, self).__init__(*args)
@@ -74,7 +107,7 @@ class ArduinoConnectPopup(Popup):
             self.size_hint = (0.4, 0.4)
         else:
             box = BoxLayout(orientation='vertical')
-            box.add_widget(Label(text='No hay equipo conectado'))
+            box.add_widget(Label(text='Verifique la conexion'))
             self.title = 'Error'
             self.content = box
             self.size_hint = (0.4, 0.4)
@@ -90,7 +123,18 @@ class ArduinoConnectPopup(Popup):
         self.dismiss()
 
     def hola(self,*args):
-        print(args)
+        puerto_arduino = args[0]
+        print(puerto_arduino)
+
+
+class PuntoEnGrafico(Widget):
+
+    posicion_x = NumericProperty(0)
+    posicion_y = NumericProperty(0)
+    posicion = ReferenceListProperty(posicion_x, posicion_y)
+
+    def move(self):
+        self.pos = Vector(self.posicion) + self.pos
 
 
 class MainApp(App):
